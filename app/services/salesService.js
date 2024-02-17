e8e7b9d64740809375872ded950b3523672fdeba
Dany Daniel Oblitas <dany.oblitas1984@gmail.com>
2024-02-17 00:41:43 -0500

    first commit - env:development conecction DB OK

---
+++ app/services/salesService.js
@@ -0,0 +1,14 @@
+/**
+ El archivo "salesService.js" es un archivo que podría contener la lógica de negocio relacionada con el módulo de ventas de la aplicación ERP. 
+ Su funcionalidad principal sería la de proporcionar una capa de servicios que encapsula la lógica de manipulación de datos y operaciones 
+ relacionadas con las ventas.
+Algunas de las funcionalidades que podrían estar presentes en "salesService.js" incluyen:
+Creación, lectura, actualización y eliminación (CRUD) de datos de ventas en la base de datos.
+Lógica de cálculo de totales, impuestos, descuentos u otros aspectos financieros relacionados con las ventas.
+Validaciones de datos antes de realizar operaciones en la base de datos.
+Integración con otros servicios o sistemas externos relacionados con las ventas.
+Implementación de reglas de negocio específicas para el módulo de ventas, como validaciones de inventario, verificación de crédito, entre otros.
+En resumen, "salesService.js" actúa como una capa intermedia entre los controladores y los modelos, encapsulando la lógica de negocio relacionada 
+con las ventas y proporcionando una interfaz coherente para realizar operaciones relacionadas con las ventas en la aplicación ERP.
+ * 
+ */
---
+++ app/utils/database.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require('sequelize');
+const config = require('../../config/config.json')['development'];
+
+const sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    {
+        host: config.host,
+        dialect: config.dialect,       
+    }
+);
+
+async function connectionTest(){
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.")
+    } catch(err){
+        console.error("Unable to connect to the database:", err)
+    }
+}
+
+connectionTest();
